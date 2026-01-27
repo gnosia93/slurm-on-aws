02b00e932c325752436289250dbb3d72fb873891
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-01-28 01:09:23 +0900

    Update variables.tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,34 +1,20 @@
-variable "aws_region" {
-  default = "ap-northeast-2"
+variable "aws_region" { default = "ap-northeast-2" }
+variable "key_name"   { type = string }
+variable "my_ip"      { description = "관리자 공인 IP (x.x.x.x/32)" }
+
+# 노드 타입 및 개수
+variable "instance_types" {
+  default = {
+    bastion    = "t3.micro"
+    master     = "t3.medium"
+    accounting = "t3.medium"
+    client     = "t3.small"
+    monitor    = "t3.medium"
+    cpu_worker = "r7iz.large"
+    gpu_worker = "g4dn.xlarge"
+  }
 }
 
-variable "key_name" {
-  description = "AWS Key Pair Name"
-  type        = string
-}
-
-# 인스턴스 타입 설정
-variable "master_instance_type" {
-  default = "t3.medium"
-}
-
-variable "cpu_worker_instance_type" {
-  default = "r7iz.large" # Intel AMX 지원
-}
-
-variable "gpu_worker_instance_type" {
-  default = "g4dn.xlarge" # NVIDIA T4
-}
-
-# 인스턴스 개수 설정
-variable "cpu_worker_count" {
-  default = 4
-}
-
-variable "gpu_worker_count" {
-  default = 4
-}
-
-variable "ami_id" {
-  default = "ami-0c02fb55956c7d316" # Amazon Linux 2023
-}
+variable "cpu_count" { default = 4 }
+variable "gpu_count" { default = 4 }
+variable "ami_id"    { default = "ami-0c02fb55956c7d316" } # AL2023
