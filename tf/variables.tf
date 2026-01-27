e6c7b865fef98bdfe81be380ce47ec709a057b27
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-01-28 01:16:56 +0900

    Update variables.tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,19 +1,15 @@
 variable "aws_region" {
-  default = "ap-northeast-2"
+  description = "AWS 배포 리전"
+  default     = "ap-northeast-2"
 }
 
 variable "key_name" {
-  description = "AWS 콘솔에 등록된 SSH 키 페어 이름"
+  description = "EC2 접속용 SSH 키 페어 이름"
   type        = string
 }
 
-variable "my_ip" {
-  description = "관리자 접속 허용 공인 IP (예: 1.2.3.4/32)"
-  type        = string
-}
-
-# 인스턴스 타입 설정
 variable "instance_types" {
+  type = map(string)
   default = {
     bastion    = "t3.micro"
     master     = "t3.medium"
@@ -25,7 +21,6 @@ variable "instance_types" {
   }
 }
 
-# 노드 대수 설정
 variable "cpu_node_count" {
   default = 4
 }
