78f5642bdc082b37eedc6538f4e3420b87acaf78
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-02-06 00:05:18 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -132,7 +132,7 @@ resource "aws_instance" "bastion" {
   subnet_id              = aws_subnet.public.id
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.slurm_sg.id]
-  tags                   = { Name = "vs-code" }
+  tags                   = { Name = "slurm-vscode" }
 
   user_data = <<_DATA
 #!/bin/bash
