35a6dcc93d672a635087e02ac629dee4b261f011
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-03-10 23:17:15 +0900

    Update main.tf

--- iac/tf/main.tf
+++ iac/tf/main.tf
@@ -69,13 +69,13 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "public" {
-  count          = length(data.aws_availability_zones.available.names)
+  count          = 1
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  count          = length(data.aws_availability_zones.available.names)
+  count          = 1
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
