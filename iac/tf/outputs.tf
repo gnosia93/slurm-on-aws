e81b4c055716fd778219af2f31bb3a6fbea30420
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-03-14 13:20:54 +0900

    Update outputs.tf

--- iac/tf/outputs.tf
+++ iac/tf/outputs.tf
@@ -2,3 +2,11 @@ output "slurm_vscode" {
     value = "http://${aws_instance.x86_box.public_dns}:9090"
     description = "브라우저에서 VS Code 서버에 접속할 수 있는 URL (PW: 'password' by default)"
 }
+
+output "slurm_monitor" {
+    value = "http://${aws_instance.x86_monitor.public_dns}:9090"
+    description = "브라우저에서 VS Code 서버에 접속할 수 있는 URL (PW: 'password' by default)"
+}
+
+
+
